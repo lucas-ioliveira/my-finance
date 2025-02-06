3fe12ddbb8bee49f2ee9f44535dbbc3cbcc0767d
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-06 20:53:37 -0300

    feat: Add total de receitas.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,22 @@
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.db.models import Sum
 
-class DashboardView(LoginRequiredMixin,TemplateView):
-    template_name = 'dashboard.html'
+from datetime import datetime, timedelta
+
+from wallet.models import Revenue
+
+@method_decorator(login_required, name='dispatch')
+class DashboardView(View):
+    def get(self, request):
+        data_limite = datetime.now() - timedelta(days=30)
+
+        revenues_total = Revenue.objects.filter(user=request.user, active=True, created_at__gte=data_limite).aggregate(total_revenues=Sum('amount'))
+        
+        context = {
+            'revenues_total': revenues_total['total_revenues']
+        }
+        return render(request, 'dashboard.html', context)
