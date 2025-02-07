0cb7915dabb5769dbd62115f7bd816e293895e7d
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-07 10:09:41 -0300

    refactor: Remoção de imports que não estão sendo utilizados.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,5 @@
 from django.views import View
-from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.db.models import Sum
@@ -14,12 +13,12 @@ class DashboardView(View):
     def get(self, request):
         data_limite = datetime.now() - timedelta(days=30)
 
-        print(data_limite)
-
         revenues_total = Revenue.objects.filter(user=request.user, active=True, payment_date__gte=data_limite).aggregate(total_revenues=Sum('amount'))
         expense_total = Expense.objects.filter(user=request.user, active=True, due_date__gte=data_limite).aggregate(total_expenses=Sum('amount'))
+
         context = {
             'revenues_total': revenues_total['total_revenues'],
             'expense_total': expense_total['total_expenses']
         }
+        
         return render(request, 'dashboard.html', context)
