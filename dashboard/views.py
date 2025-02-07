bde82638c939e85aa4ad9873d042e1338ac11654
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-07 11:41:18 -0300

    feat: Add total de investimentos no dash

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,7 @@ from django.db.models import Sum
 
 from datetime import datetime, timedelta
 
-from wallet.models import Revenue, Expense
+from wallet.models import Revenue, Expense, Investments
 
 @method_decorator(login_required, name='dispatch')
 class DashboardView(View):
@@ -15,10 +15,12 @@ class DashboardView(View):
 
         revenues_total = Revenue.objects.filter(user=request.user, active=True, payment_date__gte=data_limite).aggregate(total_revenues=Sum('amount'))
         expense_total = Expense.objects.filter(user=request.user, active=True, due_date__gte=data_limite).aggregate(total_expenses=Sum('amount'))
+        investments_total = Investments.objects.filter(user=request.user, active=True, investment_date__gte=data_limite).aggregate(total_investments=Sum('amount'))
 
         context = {
             'revenues_total': revenues_total['total_revenues'],
-            'expense_total': expense_total['total_expenses']
+            'expense_total': expense_total['total_expenses'],
+            'investments_total': investments_total['total_investments'],
         }
         
         return render(request, 'dashboard.html', context)
