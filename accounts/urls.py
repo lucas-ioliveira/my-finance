3813e3d974516b6ed145afa179d2dee716cbcb74
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-07 16:28:41 -0300

    refactor: Ajustes gerais.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, reverse_lazy
+from django.urls import path
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 
@@ -7,10 +7,7 @@ from accounts import views  as v
 
 urlpatterns = [
 
-    path('login/', LoginView.as_view(
-        form_class=AuthenticationForm,
-        template_name='login.html'
-    ), name='login'),
+    path('login/', LoginView.as_view(form_class=AuthenticationForm,template_name='login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
